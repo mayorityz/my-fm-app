04d5afe2526243fa8fb6b7e10325baf8248ce360
Abiodun Mayowa <mayority11@gmail.com>
2021-04-29 11:31:11 +0100

    update

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
@@ -6,136 +7,27 @@ import {
   AiOutlineFire,
   AiOutlineLogin,
   AiOutlineUserAdd,
+  AiOutlineMenu,
+  AiOutlineClose,
 } from "react-icons/ai";
 
 export default function Navigation() {
+  const [toggle, setToggle] = useState(false);
+  let drawer = () => setToggle(!toggle);
   return (
     <>
-      <nav class="panel-menu" id="mobile-menu">
-        <div class="mmpanels">
-          <div class="mmpanel mmopened mmcurrent" id="mm0">
-            <ul>
-              <li class="mm-close-parent">
-                <a href="#close" data-target="#close" class="mm-close">
-                  Close
-                  <div class="tt-icon">
-                    <svg>
-                      <use xlinkHref="#icon-cancel"></use>
-                    </svg>
-                  </div>
-                </a>
-              </li>
-
-              <li>
-                <a href="page-categories.html">
-                  <span>Categories</span>
-                </a>
-              </li>
-              <li>
-                <a href="page-tabs.html">
-                  <span>Trending</span>
-                </a>
-              </li>
-              <li>
-                <a href="page-create-topic.html">
-                  <span>New</span>
-                </a>
-              </li>
-              <li>
-                <a href="#mm1" data-target="#mm1" class="mm-next-level">
-                  <span>Pages</span>
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div class="mmpanel mmhidden" id="mm1">
-            <ul>
-              <li>
-                <a href="#" data-target="#" class="mm-prev-level">
-                  Back
-                </a>
-              </li>
-              <li>
-                <a href="page-single-user.html" class="mm-original-link">
-                  <span>Pages</span>
-                </a>
-              </li>
-              <li>
-                <a href="index-2.html">Home</a>
-              </li>
-              <li>
-                <a href="page-single-topic.html">Single Topic</a>
-              </li>
-              <li>
-                <a href="page-create-topic.html">Create Topic</a>
-              </li>
-              <li>
-                <a href="page-single-user.html">Single User Activity</a>
-              </li>
-              <li>
-                <a href="page-single_threads.html">Single User Threads</a>
-              </li>
-              <li>
-                <a href="page-single_replies.html">Single User Replies</a>
-              </li>
-              <li>
-                <a href="page-single_followers.html">Single User Followers</a>
-              </li>
-              <li>
-                <a href="page-single_categories.html">Single User Categories</a>
-              </li>
-              <li>
-                <a href="page-single_settings.html">Single User Settings</a>
-              </li>
-              <li>
-                <a href="page-signup.html">Sign up</a>
-              </li>
-              <li>
-                <a href="page-login.html">Log in</a>
-              </li>
-              <li>
-                <a href="page-categories.html">Categories</a>
-              </li>
-              <li>
-                <a href="page-categories-single.html">Single Category</a>
-              </li>
-              <li>
-                <a href="page-tabs.html">About</a>
-              </li>
-              <li>
-                <a href="page-tabs_guidelines.html">Guidelines</a>
-              </li>
-              <li>
-                <a href="_demo_modal-advancedSearch.html">Advanced Search</a>
-              </li>
-              <li>
-                <a href="error404.html">Error 404</a>
-              </li>
-              <li>
-                <a href="_demo_modal-age-confirmation.html">Age Verification</a>
-              </li>
-              <li>
-                <a href="_demo_modal-level-up.html">Level up Notification</a>
-              </li>
-              <li>
-                <a href="messages-page.html">Message</a>
-              </li>
-              <li>
-                <a href="messages-compose.html">Message Compose</a>
-              </li>
-            </ul>
-          </div>
+      <nav className="panel-menu mobile-main-menu" id="mobile-menu">
+        <div className="mmpanels">
+          <div className="mmpanel mmhidden" id="mm1"></div>
         </div>
       </nav>
       <header id="tt-header">
         <div className="container">
           <div className="row tt-row no-gutters">
             <div className="col-auto">
-              <a className="toggle-mobile-menu" href="#">
-                <svg className="tt-icon">
-                  <use xlinkHref="#icon-menu_icon"></use>
-                </svg>
-              </a>
+              <span className="toggle-mobile-menu">
+                <AiOutlineMenu onClick={drawer} />
+              </span>
 
               <div className="tt-logo">
                 <Link href="/">
@@ -174,6 +66,17 @@ export default function Navigation() {
               </div>
             </div>
             <div className="col-auto ml-auto">
+              <div
+                class="tt-account-btn toggle-mobile-menu"
+                style={{ padding: "1" }}
+              >
+                <a href="page-login.html" class="btn btn-primary">
+                  Log in
+                </a>
+                <a href="page-signup.html" class="btn btn-secondary">
+                  Sign up
+                </a>
+              </div>
               <div className="tt-user-info d-flex justify-content-center">
                 <div className="tt-desktop-menu">
                   <nav>
@@ -210,6 +113,28 @@ export default function Navigation() {
           </div>
         </div>
       </header>
+      <div className={!toggle ? "" : "show_menu"}>
+        <div className="close_nav_menu" onClick={drawer}>
+          <AiOutlineClose />
+        </div>
+        <ul>
+          <li>
+            <Link href="/">
+              <a>About Us</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <a>Contact Us</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <a>New Post</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
     </>
   );
 }
