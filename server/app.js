947ebd352b377917d53295a0907be04296bfc7ab
Abiodun Mayowa <mayority11@gmail.com>
2021-05-01 17:03:17 +0100

    update

--- server/app.js
+++ server/app.js
@@ -1,6 +1,44 @@
-const port = process.env.PORT || 3000;
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const Port = process.env.PORT || 3000;
 
-console.log("Running on port : ", port);
+app.use(cors());
 
-// "start": "node ./server/app.js  && next dev --port=$PORT",
-// "start": "node ./server/app.js  && next dev -p $PORT",
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.get("/tes", (req, res) => {
+  res.send("here we are!!!");
+});
+// catch undefined routes and respond with 404
+app.use((req, res, next) => {
+  res.status(404).send("Sorry can't find that!");
+});
+
+// catch server errors and respond with 500
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+// server
+//   DataBase.then(() => {
+//     console.log("db is connected");
+//     app.listen(Port, () => {
+//       console.log(`running on port:${Port}`);
+//     });
+//   }).catch((err) => {
+//     console.log(err);
+//   });
+
+app.listen(Port, () => {
+  console.log(`running on port:${Port}`);
+});
